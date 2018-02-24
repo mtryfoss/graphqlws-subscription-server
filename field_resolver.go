188c61dad28f165571d86a5bdc48130248bcf3f4
taiyoh <sun.basix@gmail.com>
2018-02-24 09:09:16 +0900

    無理に継承っぽいことをしないでもインターフェイスを渡してビルダが組み立てる、というやり方でよさそう

--- field_resolver.go
+++ field_resolver.go
@@ -6,42 +6,37 @@ import (
 	"github.com/graphql-go/graphql"
 )
 
+type GraphQLTypeResolve interface {
+	OnPayload(payload interface{}, p graphql.ResolveParams) (interface{}, error)
+	OnSubscribe(l *Listener, p graphql.ResolveParams) (interface{}, error)
+	OnUnsubscribe(l *Listener, p graphql.ResolveParams) (interface{}, error)
+}
+
 type GraphQLType interface {
-	OnPayload(interface{}, graphql.ResolveParams) (interface{}, error)
-	OnSubscribe(graphql.ResolveParams, *Listener) (interface{}, error)
-	OnUnsubscribe(graphql.ResolveParams, *Listener) (interface{}, error)
-	GetField(*Listener) *graphql.Field
+	GetResolve() GraphQLTypeResolve
 	GetType() graphql.ObjectConfig
 	GetArgs() map[string]*graphql.ArgumentConfig
 	FieldName() string
 }
 
-type GraphQLTypeImpl struct {
-	GraphQLType
-	fieldName string
-}
-
-func (t *GraphQLTypeImpl) FieldName() string {
-	return t.fieldName
-}
-
-func (t *GraphQLTypeImpl) GetField(listener *Listener) *graphql.Field {
+func BuildField(listener *Listener, t GraphQLType) *graphql.Field {
 	args := graphql.FieldConfigArgument{}
 	for name, arg := range t.GetArgs() {
 		args[name] = arg
 	}
+	resolver := t.GetResolve()
 	return &graphql.Field{
 		Type: graphql.NewObject(t.GetType()),
 		Args: args,
 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 			if payload := p.Context.Value(ListenerContextKey("payload")); payload != nil { // payload exists
-				return t.OnPayload(payload, p)
+				return resolver.OnPayload(payload, p)
 			}
 			if s := p.Context.Value(ListenerContextKey("onSubscribe")); s != nil { // AddSubscription called
-				return t.OnSubscribe(p, listener)
+				return resolver.OnSubscribe(listener, p)
 			}
 			if s := p.Context.Value(ListenerContextKey("onUnsubscribe")); s != nil { // removeSubscription called
-				return t.OnUnsubscribe(p, listener)
+				return resolver.OnUnsubscribe(listener, p)
 			}
 			return nil, errors.New("no payload exists")
 		},
