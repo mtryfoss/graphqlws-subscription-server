7741bc56b97b0dc411c9ef2fede1468e03f01ed2
taiyoh <sun.basix@gmail.com>
2018-02-25 23:28:17 +0900

    AddSubscription/RemoveSubscription(s) のテストを追加

--- subscribeservice_test.go
+++ subscribeservice_test.go
@@ -3,8 +3,35 @@ package gss
 import (
 	"testing"
 
+	"github.com/functionalfoundry/graphqlws"
 	"github.com/graphql-go/graphql"
 )
+
+type connForTest struct {
+	graphqlws.Connection
+	id              string
+	user            interface{}
+	ReceivedOpID    string
+	ReceivedPayload *graphqlws.DataMessagePayload
+	ReceivedError   error
+}
+
+func (c *connForTest) ID() string {
+	return c.id
+}
+
+func (c *connForTest) User() interface{} {
+	return c.user
+}
+
+func (c *connForTest) SendData(opID string, d *graphqlws.DataMessagePayload) {
+	c.ReceivedOpID = opID
+	c.ReceivedPayload = d
+}
+
+func (c *connForTest) SendError(e error) {
+	c.ReceivedError = e
+}
 
 func TestNewSubscribeService(t *testing.T) {
 	user := map[string]string{}
@@ -24,6 +51,13 @@ func TestNewSubscribeService(t *testing.T) {
 	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
 	schema, _ := graphql.NewSchema(schemaConfig)
 
+	// Query
+	query := `
+		{
+			hello
+		}
+	`
+
 	subChan := make(chan *SubscribeEvent, 1)
 	unsubChan := make(chan *UnsubscribeEvent, 1)
 
@@ -31,5 +65,44 @@ func TestNewSubscribeService(t *testing.T) {
 	s.SubscribeFilter()
 	s.SubscribeCalculator()
 	s.GetNotifierChan()
-	s.Subscriptions()
+
+	conn1 := &connForTest{
+		id:   "hoge",
+		user: user,
+	}
+
+	sub1 := &graphqlws.Subscription{
+		ID:            "foo",
+		Query:         query,
+		Variables:     map[string]interface{}{},
+		OperationName: "",
+		Fields:        []string{},
+		Connection:    conn1,
+		SendData:      func(d *graphqlws.DataMessagePayload) {},
+	}
+
+	errs := s.AddSubscription(conn1, sub1)
+	if len(errs) > 0 {
+		t.Error("errors should not exists")
+	}
+
+	subs := s.Subscriptions()
+	if _, exists := subs[conn1]; !exists {
+		t.Error("conn1 should exists")
+	}
+
+	s.RemoveSubscription(conn1, sub1)
+	subs = s.Subscriptions()
+	if _, exists := subs[conn1]; exists {
+		t.Error("conn1 should not exists")
+	}
+
+	s.AddSubscription(conn1, sub1)
+	s.RemoveSubscriptions(conn1)
+
+	subs = s.Subscriptions()
+	if _, exists := subs[conn1]; exists {
+		t.Error("conn1 should not exists")
+	}
+
 }
