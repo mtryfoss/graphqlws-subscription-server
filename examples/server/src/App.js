6df6351e53b79cdd6b2107891fad41ec269d130a
taiyoh <sun.basix@gmail.com>
2018-03-18 23:02:04 +0900

    隠そうとしている

--- examples/server/src/App.js
+++ examples/server/src/App.js
@@ -4,6 +4,8 @@ import Header from './Header';
 import Name, { NameWatcher } from './Name';
 import { NewChannel, ChannelList, ChannelWatcher } from './Channel';
 import Notification, { NotificationWatcher } from './Notification';
+import styled, { css } from 'styled-components';
+import { inject, observer } from 'mobx-react';
 
 const nameWatcher = new NameWatcher()
 const chanWatcher = new ChannelWatcher();
@@ -15,12 +17,20 @@ nameWatcher.onRegisterCallback = () => {
 
 const theme = createMuiTheme();
 
+const AppContainer = observer(styled.div`
+  ${ props => !props.isWalkedIn && css`
+  display: none;
+  `}
+`)
+
 export default (props) => (
   <MuiThemeProvider theme={theme}>
     <Header />
     <Name watcher={nameWatcher} />
-    <Notification watcher={notifWatcher} />
-    <NewChannel watcher={chanWatcher} />
-    <ChannelList watcher={chanWatcher} />
+    <AppContainer isWalkedIn={nameWatcher.isWalkedIn}>
+      <Notification watcher={notifWatcher} />
+      <NewChannel watcher={chanWatcher} />
+      <ChannelList watcher={chanWatcher} />
+    </AppContainer>
   </MuiThemeProvider>
 )
